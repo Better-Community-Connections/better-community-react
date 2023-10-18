f6f45763e450be85e58c90161e67729299b5857e
somonkulov <sardorbek_omonkulov@innovationfirst.com>
2023-10-18 19:51:51 -0400

    Init react

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen gap-8 antialiased">
+      <p className="text-3xl mr-96">Better...</p>
+      <p className="text-5xl text-orange-500 font-medium	">Community</p>
+      <p className="text-3xl ml-96">...Connection</p>
+    </div>
+  );
+}
+
+export default App;
